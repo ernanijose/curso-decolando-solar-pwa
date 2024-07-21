c2aa10b0592301204f88d047cd72400a6c53141e
ernanijmaximize <132609996+ernanijmaximize@users.noreply.github.com>
2024-07-21 10:31:43 -0300

    Criando a pagina inicial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { ThemeProvider } from "@mui/material";
+import tema from "./Tema";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import PaginaInicio from "./paginas/inicio";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <ThemeProvider
+      theme={tema}
+      >
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<PaginaInicio />} />
+          </Routes>
+        </BrowserRouter>
+      </ThemeProvider>
+    </>
   );
 }
 
